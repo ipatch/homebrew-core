9bf6a6d3ad019fd1dda65132053c95e7c0b272d5
ipatch <chris.r.jones.1983@gmail.com>
2018-01-01 20:27:36 -0600

    weechat was not building due to having mulitiple pythons 🐍 in the path.

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -28,12 +28,13 @@ class Weechat < Formula
   depends_on :ruby => ["2.1", :optional]
   depends_on :perl => ["5.3", :optional]
   depends_on "curl" => :optional
-  depends_on "php71" if build.with? "python"
-  depends_on "php71" if build.with? "python3"
+  # depends_on "php71" if build.with? "python"
+  # depends_on "php71" if build.with? "python3"
+  # depends_on :python => :optional
 
-  if MacOS.version >= :mavericks
-    option "with-custom-python", "Build with a custom Python 2 instead of the Homebrew version."
-  end
+  # if MacOS.version >= :mavericks
+  #   option "with-custom-python", "Build with a custom Python 2 instead of the Homebrew version."
+  # end
 
   depends_on :python => :optional
   depends_on :python3 => :optional
@@ -45,42 +46,140 @@ class Weechat < Formula
 
     # -CMAKE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}
 
-    ENV.delete("PYTHONPATH")
+    # -DCMAKE_LIBRARY_PATH=/usr/local/Cellar/php71/7.1.12_23/libexec/apache2/
+
+    # ENV.delete("PYTHONPATH")
 
     args = std_cmake_args + %W[
       -DENABLE_GUILE=OFF
       -DCA_FILE=#{etc}/openssl/cert.pem
       -DENABLE_JAVASCRIPT=OFF
-
+      -DENABLE_PHP=OFF
     ]
     if build.with? "debug"
       args -= %w[-DCMAKE_BUILD_TYPE=Release]
       args << "-DCMAKE_BUILD_TYPE=Debug"
+      # args -= %w[-DCMAKE_LIBRARY_PATH=]
+      # args << "-DCMAKE_LIBRARY_PATH=/usr/local/Cellar/php71/7.1.12_23/libexec/apache2/"
     end
 
     # Allow python or python3, but not both; if the optional
     # python is chosen, default to it; otherwise, use python3
 
-    # NOTE: weechat still prefers python 2 supprot because
+    # NOTE: weechat still prefers python 2 supprot
     # because many scripts are not compatible with python 3.
 
-    if build.with? "python"
-      ENV.prepend "LDFLAGS", `python-config --ldflags`.chomp
+    ##
+    # /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula
+    ##
 
-      framework_script = <<~EOS
-        import sysconfig
-        print sysconfig.get_config_var("PYTHONFRAMEWORKPREFIX")
-      EOS
-      framework_prefix = `python -c '#{framework_script}'`.strip
-      # Non-framework builds should have PYTHONFRAMEWORKPREFIX defined as ""
-      if framework_prefix.include?("/") && framework_prefix != "/System/Library/Frameworks"
-        ENV.prepend "LDFLAGS", "-F#{framework_prefix}"
-        ENV.prepend "CFLAGS", "-F#{framework_prefix}"
-      end
-      args << "-DENABLE_PYTHON=ON"
-    elsif build.with? "python"
-      args << "-DENABLE_PYTHON3=ON"
+    # /usr/local/Cellar/python/2.7.14/Frameworks/Python.framework/Versions/2.7/include/python2.7/Python.h
+
+    if build.with?("python") && build.with?("python3")
+      odie "weechat: Does not support building both python 2 and 3 plugins"
+    elsif build.with?("python")
+      ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+
+      bin.install Dir[libexec/"bin/*"]
+      bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+      # ENV.prepend_create_path "PYTHONPATH", "/Library/Python/2.7/site-packages"
+      # # xy = Language::Python.major_minor_version "python"
+      # # ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+      # # system "python", *Language::Python.setup_install_args(libexec)
+      #
+      # # bin.install Dir[libexec/"bin/*"]
+      # bin.env_script_all_files("/bin", :PYTHONPATH => ENV["PYTHONPATH"])
+
+
+      # ENV.prepend_path "PYTHONPATH", libexec
+      # bin.env_script_all_files(libexec, :PYTHONPATH => ENV["PYTHONPATH"])
+
+
+      # ENV["PYTHON"]=/usr/bin/python
+
+      # ENV.prepend_create_path "PYTHONPATH", "#{libexec}/lib/python2.7/site-packages"
+
+      # system "python", *Language::Python.setup_install_args(libexec)
+
+      # bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+      # ENV["PYTHONPATH"] = libexec/"lib/python2.7/site-packages"
+
+      # xy = Language::Python.major_minor_version "python"
+      # ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
+
+      # python_executable = `!/usr/bin/python`.strip if build.with? "python"
+      # python_prefix = `#{python_executable} -c 'import sys;print(sys.prefix)'`.chomp
+      # python_include = `#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'`.chomp
+      #
+      # # cmake picks up the system's python dylib, even if we have a brewed one
+      # if File.exist? "#{python_prefix}/Python"
+      #   python_library = "#{python_prefix}/Python"
+      # elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
+      #   python_library = "#{python_prefix}/lib/lib#{python_version}.a"
+      # elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.dylib"
+      #   python_library = "#{python_prefix}/lib/lib#{python_version}.dylib"
+      # else
+      #   odie "No libpythonX.Y.{a,dylib} file found!"
+      # end
+      # args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
+      # args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+      # args << "-DPYTHON_LIBRARY='#{python_library}'"
+    elsif build.with?("python3")
+      # python_executable = `which python3`.strip if build.with? "python3"
+      # python_prefix = `#{python_executable} -c 'import sys;print(sys.prefix)'`.chomp
+      # python_include = `#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'`.chomp
+      # python_version = "python" + `#{python_executable} -c 'import sys;print(sys.version[:3])'`.chomp
+      #
+      # # cmake picks up the system's python dylib, even if we have a brewed one
+      # if File.exist? "#{python_prefix}/Python"
+      #   python_library = "#{python_prefix}/Python"
+      # elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
+      #   python_library = "#{python_prefix}/lib/lib#{python_version}.a"
+      # elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.dylib"
+      #   python_library = "#{python_prefix}/lib/lib#{python_version}.dylib"
+      # else
+      #   odie "No libpythonX.Y.{a,dylib} file found!"
+      # end
+      # args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
+      # args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+      # args << "-DPYTHON_LIBRARY='#{python_library}'"
     end
+
+    if build.with?("python")
+      args << "-DENABLE_PYTHON=ON"
+    else
+      args << "-DENABLE_PYTHON=OFF"
+    end
+
+    if build.with?("python3")
+      args << "-DENABLE_PYTHON3=ON"
+    else
+      args << "-DENABLE_PYTHON3=OFF"
+    end
+
+
+
+    # if build.with? "python"
+    #   args -= %w[-DCMAKE_LIBRARY_PATH=]
+    #   args << "-DCMAKE_LIBRARY_PATH=/usr/local/Cellar/php71/7.1.12_23/libexec/apache2/"
+    #   ENV.prepend "LDFLAGS", `python-config --ldflags`.chomp
+    #
+    #   framework_script = <<~EOS
+    #     import sysconfig
+    #     print sysconfig.get_config_var("PYTHONFRAMEWORKPREFIX")
+    #   EOS
+    #   framework_prefix = `python -c '#{framework_script}'`.strip
+    #   # Non-framework builds should have PYTHONFRAMEWORKPREFIX defined as ""
+    #   if framework_prefix.include?("/") && framework_prefix != "/System/Library/Frameworks"
+    #     ENV.prepend "LDFLAGS", "-F#{framework_prefix}"
+    #     ENV.prepend "CFLAGS", "-F#{framework_prefix}"
+    #   end
+    #   args << "-DENABLE_PYTHON=ON"
+    # elsif build.with? "python3"
+    #   args -= %w[-DCMAKE_LIBRARY_PATH=]
+    #   args << "-DCMAKE_LIBRARY_PATH=/usr/local/Cellar/php71/7.1.12_23/libexec/apache2/"
+    #   args << "-DENABLE_PYTHON3=ON"
+    # end
 
     args << "-DENABLE_LUA=OFF" if build.without? "lua"
     args << "-DENABLE_PERL=OFF" if build.without? "perl"
