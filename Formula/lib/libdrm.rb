654bc5588681cebcaaa46e18d443fb838635236a
chris <chris.r.jones.1983@gmail.com>
2024-09-28 15:28:03 -0500

    libdrm: tshoot: for some reason `$bp/lib/python3.12/site-packages` is not being found

--- Formula/lib/libdrm.rb
+++ Formula/lib/libdrm.rb
@@ -1,5 +1,5 @@
 class Libdrm < Formula
-  include Language::Python::Virtualenv
+  # include Language::Python::Virtualenv
 
   desc "Library for accessing the direct rendering manager"
   homepage "https://dri.freedesktop.org"
@@ -24,9 +24,13 @@ class Libdrm < Formula
   depends_on "libpciaccess"
   depends_on :linux
 
-  resource "meson" do
-    url "https://files.pythonhosted.org/packages/34/e8/bb0e264882a42f5d5acae869d5980f298ff9c298d844e18fd1ac009ce7e9/meson-1.5.2.tar.gz"
-    sha256 ""
+  # resource "meson" do
+  #   url "https://files.pythonhosted.org/packages/34/e8/bb0e264882a42f5d5acae869d5980f298ff9c298d844e18fd1ac009ce7e9/meson-1.5.2.tar.gz"
+  #   sha256 ""
+  # end
+
+  def python3
+    "python3.12"
   end
 
   def install
@@ -34,6 +38,21 @@ class Libdrm < Formula
     # ModuleNotFoundError: No module named 'mesonbuild'
     # virtualenv_install_with_resources :using => "python@3.12"
 
+    # NO WORK!
+    # system "python3", "meson", "setup", "build", "-Dcairo-tests=disabled", "-Dvalgrind=disabled", *std_meson_args
+
+    puts "-----------------------------------------------------------------"
+    puts "PYTHONPATH: #{`python3.12 -c 'import site; print(site.getsitepackages()[0])'`}"
+    puts "-----------------------------------------------------------------"
+
+    # system "python3", "-c", "import mesonbuild; print('mesonbuild module is available')"
+    puts "-----------------------------------------------------------------"
+
+    # TODO: ipatch, why is the below path not found / added
+    # TODO: do not hardcode path entries, mk more robust ie. less brittle
+    ENV.prepend_path "PYTHONPATH", "/home/capin/homebrew/lib/python3.12/site-packages"
+
+    # system "meson", "setup", "build", "-Dcairo-tests=disabled", "-Dvalgrind=disabled"
     system "meson", "setup", "build", "-Dcairo-tests=disabled", "-Dvalgrind=disabled", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
