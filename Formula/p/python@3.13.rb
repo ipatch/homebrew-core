423a04012a8a57013f9afa5655adb8881d810a28
chris <chris.r.jones.1983@gmail.com>
2024-10-11 14:29:10 -0500

    python@3.13: asahi linux, working build install ☑️

--- Formula/p/python@3.13.rb
+++ Formula/p/python@3.13.rb
@@ -43,6 +43,8 @@ class PythonAT313 < Formula
     depends_on "berkeley-db@5"
     depends_on "libnsl"
     depends_on "libtirpc"
+    depends_on "tcl-tk"
+    depends_on "util-linux"
   end
 
   link_overwrite "bin/2to3"
